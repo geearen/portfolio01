577c44204958ffd4b2b84fa4f00b6691aaaa724f
geearen <allengonzales018@gmail.com>
2021-09-27 15:39:27 -0700

    added background particles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Background from './components/Background/Background';
 
 function App() {
   return (
     <main>
+      <Background />
       Hello
     </main>
   );
