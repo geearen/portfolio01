bead69b51726b94062f8d985442a1e26bd2c15c7
geearen <allengonzales018@gmail.com>
2021-09-27 11:15:17 -0700

    checking

--- api/date.go
+++ api/date.go
@@ -1,12 +1,12 @@
-// package handler
+package handler
 
-// import (
-// 	"fmt"
-// 	"net/http"
-// 	"time"
-// )
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// 	currentTime := time.Now().Format(time.RFC850)
-// 	fmt.Fprintf(w, currentTime)
-// }
+func Handler(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Format(time.RFC850)
+	fmt.Fprintf(w, currentTime)
+}
