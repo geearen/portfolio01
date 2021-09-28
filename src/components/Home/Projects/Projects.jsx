5e912f797add592ce8933c1273adc66617687d1b
geearen <allengonzales018@gmail.com>
2021-09-28 10:09:47 -0700

    add projects detail

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { ProjectData } from '../../data/ProjectData'
 
 export default function Projects() {
   return (
-    <div>
-      Projects Here
-    </div>
+    <>
+      {ProjectData.map((item,index) =>{
+        return(
+          <div key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+
+            <div>
+              <a href={item.deployed}>
+                View Site
+              </a>
+              <a href={item.github}>
+                GitHub
+              </a>
+            </div>
+          </div>
+        )
+      })}
+    </>
   )
 }
---
+++ src/components/data/ProjectData.js
@@ -0,0 +1,34 @@
+export const ProjectData = [
+  {
+    key: 0,
+    className: "project1",
+    title: "Jujutsu Kaisen-Gotchi",
+    description: "",
+    deployed: "https://geearen.github.io/Jujutsu-Kaisen-Gotchi/",
+    github: "https://github.com/geearen/Jujutsu-Kaisen-Gotchi",
+  },
+  {
+    key: 1,
+    className: "project2",
+    title: "Goodeats",
+    description: "",
+    deployed: "https://goodeats-allen-nick.herokuapp.com/",
+    github: "https://github.com/geearen/Goodeats",
+  },
+  {
+    key: 2,
+    className: "project3",
+    title: "The Wayfarer",
+    description: "",
+    deployed: "https://the-number-one-wayfarer.herokuapp.com/",
+    github: "https://github.com/geearen/the-wayfarer",
+  },
+  {
+    key: 3,
+    className: "project4",
+    title: "FotoNegative",
+    description: "",
+    deployed: "https://fotonegative.herokuapp.com/",
+    github: "https://github.com/geearen/FotoNegative",
+  },
+];
