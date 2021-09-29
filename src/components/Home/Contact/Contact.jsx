4317a08d57490e65f4bb5aff009951995091f709
geearen <allengonzales018@gmail.com>
2021-09-29 00:18:40 -0700

    quick fix with icons

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -3,16 +3,15 @@ import React from 'react'
 export default function Contact() {
   return (
     <div className="contact-section">
-
       <div className="contact-content uk-card uk-card-default">
-        <h3 className="uk-card-title"> Contact Info Here: </h3>
+        <h2 className="title"> Contact Info Here: </h2>
 
         <a
           href="mailto:allengonzales018@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span uk-icon="icon: mail"></span>allengonzales018@gmail.com
+          <span uk-icon="icon: mail; ratio:3"></span>allengonzales018@gmail.com
         </a>
 
         <a
@@ -20,21 +19,21 @@ export default function Contact() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span uk-icon="icon: linkedin"></span>/in/allen-gee
+          <span uk-icon="icon: linkedin; ratio:3"></span>/in/allen-gee
         </a>
         <a
           href="https://github.com/geearen"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span uk-icon="icon: github"></span>geearen
+          <span uk-icon="icon: github; ratio:3"></span>geearen
         </a>
         <a
           href="https://www.instagram.com/geearen.film/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span uk-icon="icon: instagram"></span>geearen.film
+          <span uk-icon="icon: instagram; ratio:3"></span>geearen.film
         </a>
       </div>
     </div>
