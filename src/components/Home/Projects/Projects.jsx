4d63b24c56a0d95453a7f02e2f8e02b4797420c1
geearen <allengonzales018@gmail.com>
2021-09-30 13:52:29 -0700

    add styling on projects sections

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import { ProjectData } from '../../data/ProjectData'
+import React from "react";
+import { ProjectData } from "../../data/ProjectData";
 
 export default function Projects() {
   return (
     <>
-      {ProjectData.map((item,index) =>{
+      {ProjectData.map((item, index) => {
         return (
           <div key={index} className="container">
-            <div className="project-container">
-              <div>
+            <div className={`${item.className} project-container`}>
+              <div className="meta-info">
                 <h3>{item.title}</h3>
                 <p>{item.description}</p>
-              </div>
 
-              <div>
-                <a
-                  href={item.deployed}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Site
-                </a>
-                <a href={item.github} 
-                target="_blank" 
-                rel="noopener noreferrer">
-                  GitHub
-                </a>
+                
+                  <a
+                    href={item.deployed}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span data-uk-icon="icon: link"></span>
+                    Live Demo
+                  </a>
+                  <a
+                    href={item.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span data-uk-icon="icon: github"></span>
+                    GitHub
+                  </a>
+                
               </div>
             </div>
           </div>
         );
       })}
     </>
-  )
+  );
 }
