8d80dacff5ece87ce875dfe457acd57a5cee57b5
geearen <allengonzales018@gmail.com>
2021-12-06 17:13:53 -0800

    adds outlet to project section

--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -1,11 +1,21 @@
 export const ProjectData = [
   {
+    key: 4,
+    className: "project5",
+    title: "Outlet",
+    description:
+      "Outlet is a responsive drawing app where you can create art of your own. Draw with pen and fun shapes, save to your computer, clear, and draw again!",
+    tech: "React and SASS",
+    deployed: "https://outlet.vercel.app//",
+    github: "https://github.com/geearen/Outlete",
+  },
+  {
     key: 3,
     className: "project4",
     title: "FotoNegative",
     description:
-    "FotoNegative is a full stack application targeting on photographers and hobbyists, where it allows users to views different types of camera from film to digital. Inspired by KenRockwell.com and Instagram",
-    tech:"Node.js, Express.js, MongoDB, SASS, UIkit, AWS S3 and Unsplash API",
+      "FotoNegative is a full stack application targeting on photographers and hobbyists, where it allows users to views different types of camera from film to digital. Inspired by KenRockwell.com and Instagram",
+    tech: "Node.js, Express.js, MongoDB, SASS, UIkit, AWS S3 and Unsplash API",
     deployed: "https://fotonegative.herokuapp.com/",
     github: "https://github.com/geearen/FotoNegative",
   },
@@ -14,8 +24,8 @@ export const ProjectData = [
     className: "project3",
     title: "The Wayfarer",
     description:
-    "Wayfarer is a travel app aimed specifically at enabling users to post nightlife stories and tips for the cities of their choice.",
-    tech:"Python, Django, PostgreSQL, CSS, jQuery and Skeleton",
+      "Wayfarer is a travel app aimed specifically at enabling users to post nightlife stories and tips for the cities of their choice.",
+    tech: "Python, Django, PostgreSQL, CSS, jQuery and Skeleton",
     deployed: "https://the-number-one-wayfarer.herokuapp.com/",
     github: "https://github.com/geearen/the-wayfarer",
   },
@@ -24,8 +34,8 @@ export const ProjectData = [
     className: "project2",
     title: "Goodeats",
     description:
-    "Goodeats is a recipe website where user get to share their recipes.",
-    tech:"Node.js, Express.js, MongoDB, SASS, Semantic UI and jQuery",
+      "Goodeats is a recipe website where user get to share their recipes.",
+    tech: "Node.js, Express.js, MongoDB, SASS, Semantic UI and jQuery",
     deployed: "https://goodeats-allen-nick.herokuapp.com/",
     github: "https://github.com/geearen/Goodeats",
   },
@@ -35,7 +45,7 @@ export const ProjectData = [
     title: "Jujutsu Kaisen-Gotchi",
     description:
       "Jujutsu Kaisen-Gotchi was my first project I built as a part of General Assembly's Software Engineering Immersive Program.",
-    tech:"HTML, CSS, JavaScript, Animate.CSS, and NES CSS Framework",
+    tech: "HTML, CSS, JavaScript, Animate.CSS, and NES CSS Framework",
     deployed: "https://geearen.github.io/Jujutsu-Kaisen-Gotchi/",
     github: "https://github.com/geearen/Jujutsu-Kaisen-Gotchi",
   },
