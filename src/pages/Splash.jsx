4bd5d45c2cba1d01397c5d8858eb1c19bfe76b57
geearen <allengonzales018@gmail.com>
2021-12-20 22:49:25 -0800

    restyled splash page; adds a small animation

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Splash() {
   return (
-    <div className="splash">
-      <div className="left-section">
-        <img
-          src="https://res.cloudinary.com/dsxis0hux/image/upload/v1632948791/pics/Open_Peeps_-_Bust_im9dc9.png"
-          alt="splash image"
-        />
-      </div>
-      <div className="right-section">
-        <h1>
-          Hi, I'm Allen Gonzales 👋
-          <br /> Software Engineer & I love coffee ☕
-        </h1>
-        <a href="/home"> ~ yarn create-react-app my-portfolio &#8626; </a>
+    <div className="background-grad-splash">
+      <div className="splash">
+        <div className="left-section">
+          <img
+            src="https://res.cloudinary.com/dsxis0hux/image/upload/v1632948791/pics/Open_Peeps_-_Bust_im9dc9.png"
+            alt="splash image"
+          />
+        </div>
+        <div className="right-section">
+          <h1>
+            Hi, I'm Allen Gonzales 👋
+            <br /> Software Engineer & I love coffee ☕
+          </h1>
+          <a href="/home"> ~ yarn create-react-app my-portfolio &#8626; </a>
+        </div>
       </div>
     </div>
   );
