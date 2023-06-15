34e041c0c7540414b8526090c8063de9e2d57fa3
geearen <allengonzales018@gmail.com>
2023-06-15 16:00:05 -0700

    added a footer

--- src/components/Components/PageContainer/PageContainer.jsx
+++ src/components/Components/PageContainer/PageContainer.jsx
@@ -1,11 +1,11 @@
+import Footer from "../Footer/Footer";
 import Nav from "../NavBar/Nav";
 const PageContainer = ({ children }) => {
   return (
     <>
       <Nav />
-      <div className="page-container">
-        {children}
-      </div>
+      <div className="page-container">{children}</div>
+      <Footer />
     </>
   );
 };
