49bd5aed65422b1fa3eb603ac5332a79facd9bd6
geearen <allengonzales018@gmail.com>
2023-06-15 16:56:25 -0700

    added a message on photo pages - currently under construction

---
+++ src/components/Components/PhotoGallery/PhotoGallery.jsx
@@ -0,0 +1,16 @@
+const PhotoGallery = () => {
+  return (
+    <div className="photos-container">
+      <h1>Photos</h1>
+      <div className="circle">
+        <h1>
+          <span>Under</span>
+          <br />
+          <span>Construction</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoGallery;
--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,7 +1,8 @@
 import PageContainer from "../components/Components/PageContainer/PageContainer";
+import PhotoGallery from "../components/Components/PhotoGallery/PhotoGallery";
 
 const Photo = () => {
-  return <PageContainer children={<div> Photo </div>}></PageContainer>;
-}
+  return <PageContainer children={<PhotoGallery />}></PageContainer>;
+};
 
 export default Photo;
