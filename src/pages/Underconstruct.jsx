182c9ba5a2197ca43e1bb9f04c5ac29977a08f2a
geearen <allengonzales018@gmail.com>
2026-02-19 19:31:37 -0800

    update packages, added new components/pages and update routing

---
+++ src/pages/Underconstruct.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../components/Components/About/About";
+import Footer from "../components/Components/Footer/Footer";
+import Contact from "../components/Components/Contact/Contact";
+import PageContainer from "../components/Components/PageContainer/PageContainer";
+import Underconstruct from "../components/Components/Underconstruct/Underconstruct";
+
+function UnderconstructPage() {
+  return <Underconstruct />;
+}
+
+export default UnderconstructPage;
